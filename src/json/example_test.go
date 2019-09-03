c4ebbd34bb4943e8a349805dd81961bd4bcc7724
Opemipo <ogunks@live.com>
2019-09-03 07:36:30 +0100

    Update example_test.go

--- src/json/example_test.go
+++ src/json/example_test.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 )
 
-// ExampleLoad demonstrates how to use Load
+// ExampleUnmarshall demonstrates how to use Load
 func ExampleUnmarshall() {
 	s := Unmarshall([]byte(`["v1", "v2"]`))
 	fmt.Println(s)
