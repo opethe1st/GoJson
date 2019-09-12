28f43887378afa9bc575f2e5a93876101aaaa535
Opemipo Ogunkola <ogunks@live.com>
2019-09-12 09:33:15 +0100

    recognize + as a number

--- src/json/unmarshall.go
+++ src/json/unmarshall.go
@@ -49,7 +49,7 @@ func unmarshallLiteral(iter *iterator, literal string, value any) any {
 
 func isNumber(iter *iterator) bool {
 	switch iter.Current() {
-	case '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '0':
+	case '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '0', '+':
 		return true
 	}
 	return false
